5929bec92f6cbf0532652ece6d415e347f5ba115
Nicholas Schuetze <nicholas.schuetze@mavs.uta.edu>
2017-01-19 17:08:11 -0600

    Menu button interaction and enable/disable teleporting

---
+++ RoomView/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+    public SteamVR_TrackedObject controller;
+    
+    public LevelManager levelManager;
+    public string loadLevel;
+
+
+    private SteamVR_TrackedController controllerInput;
+    private SteamVR_LaserPointer pointer;
+	// Use this for initialization
+	void Awake () {
+	    if(controller !=null )
+        {
+            try
+            {
+                pointer = controller.GetComponent<SteamVR_LaserPointer>();
+                controllerInput = controller.GetComponent<SteamVR_TrackedController>();
+            }
+            catch
+            {
+                Debug.LogError(this.name +": Could not find LaserPointer");
+            }
+        }
+	}
+
+    private void OnEnable()
+    {
+        pointer.PointerIn += OnHover;
+        pointer.PointerOut += OffHover;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnHover(object sender, PointerEventArgs e)
+    {
+        if(e.target == GetComponent<Collider>().transform)
+        {
+            gameObject.GetComponent<Text>().color = Color.green;
+            controllerInput.TriggerUnclicked += OnSelectButton;
+        }
+    }
+
+    void OffHover(object sender, PointerEventArgs e)
+    {
+        if(e.target == GetComponent<Collider>().transform)
+        {
+            gameObject.GetComponent<Text>().color = Color.black;
+            controllerInput.TriggerUnclicked -= OnSelectButton;
+        }
+    }
+
+    void OnSelectButton(object sender, ClickedEventArgs e)
+    {
+        levelManager.LoadLevel(loadLevel);
+    }
+}
---
+++ RoomView/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour {
+
+    private SteamVR_TrackedObject controller;
+    private SteamVR_TrackedController controllerInput;
+    private SteamVR_Teleporter teleporter;
+    private SteamVR_LaserPointer pointer;
+
+	// Use this for initialization
+	void Awake () {
+        controller = GetComponent<SteamVR_TrackedObject>();
+        controllerInput = GetComponent<SteamVR_TrackedController>();
+        teleporter = GetComponent<SteamVR_Teleporter>();
+        pointer = GetComponent<SteamVR_LaserPointer>();
+	}
+
+    void OnEnable()
+    {
+        controllerInput.PadClicked += EnableTeleport;
+        controllerInput.PadUnclicked += DisableTeleport;
+    }
+
+    void Start()
+    {
+        teleporter.teleportOnClick = false;
+        pointer.pointer.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+
+    private void EnableTeleport (object sender, ClickedEventArgs e)
+    {
+        teleporter.teleportOnClick = true;
+        pointer.pointer.GetComponent<Renderer>().enabled = true;
+    }
+
+    private void DisableTeleport (object sender, ClickedEventArgs e)
+    {
+        teleporter.teleportOnClick = false;
+        pointer.pointer.GetComponent<Renderer>().enabled = false;
+    }
+}
