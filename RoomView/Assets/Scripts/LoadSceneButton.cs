52146561a2ae92b94caf9b7f2f677418657251f2
Nicholas Schuetze <nicholas.schuetze@mavs.uta.edu>
2017-01-20 16:28:59 -0600

    Loading into Preset Room Sizes

---
+++ RoomView/Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneButton : MenuButton {
+
+    public string levelName;
+    public LevelManager levelManager;
+
+    public override void OnSelectButton(object sender, ClickedEventArgs e)
+    {
+        levelManager.LoadLevel(levelName);
+    }
+}
