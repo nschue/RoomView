c40be82e353896cf25ee0caa054d017730bd5751
Luis Diaz Jr <dluis.create@gmail.com>
2017-04-26 20:20:13 -0500

    ObjCat and OMenuClick Doxygen

--- RoomView/Assets/Scripts/OMenuClick.cs
+++ RoomView/Assets/Scripts/OMenuClick.cs
@@ -1,32 +1,36 @@
-﻿using UnityEngine;
+﻿/*!
+ * \author Luis Diaz Jr
+ * \version 1.0
+ * \date 4-17-2017
+ *
+ * \mainpage Button Controller for Context menu
+ */ 
+using UnityEngine;
 using System.Collections;
 
 public class OMenuClick : MonoBehaviour {
 
-	private UnityEngine.UI.Image activeImage;
+	private UnityEngine.UI.Image activeImage;	//!< pointer to the button (image) gameobject
 
-	private bool isOn = false;	// button status
+	private bool isOn = false;					//!< button status
 
     public SteamVR_TrackedObject controller;
-	public Sprite onSprite;		// holds the on sprite
-	public Sprite offSprite;	// holds the off sprite
-    private SteamVR_LaserPointer pointer;
+	public Sprite onSprite;						//!< pointer to the on sprite
+	public Sprite offSprite;					//!< pointer to the off sprite
+    private SteamVR_LaserPointer pointer;		//!< pointer to the laser pointer
 
 
-    // Use this for initialization
+    /*!
+	 * \brief Runs upon gameobject spawn (at start) and initializes image GO and pointer
+	 */
     void Start () {
 		activeImage = GetComponent<UnityEngine.UI.Image>(); // get the image component
         pointer = controller.GetComponent<SteamVR_LaserPointer>();
-
- 
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	// changes the sprite according to status
+	/*!
+	 * \brief Toggles the button on/off
+	 */
 	public void swapSprite(){
 		if (isOn){
 			activeImage.sprite = offSprite;
@@ -38,7 +42,9 @@ public class OMenuClick : MonoBehaviour {
 		}
 	}
 
-	// sets the off sprite and flag
+	/*!
+	 * \brief sets the off sprite and flag
+	 */
 	public void turnOff() {
 		if (!isOn)
 			return;
@@ -47,7 +53,9 @@ public class OMenuClick : MonoBehaviour {
 		isOn = false;
 	}
 
-	//sets the on sprite and flag
+	/*!
+	 * \brief sets the on sprite and flag
+	 */
 	public void turnOn() {
 		if (isOn)
 			return;
--- RoomView/Assets/Scripts/ObjectCategory.cs
+++ RoomView/Assets/Scripts/ObjectCategory.cs
@@ -1,24 +1,35 @@
-﻿using UnityEngine;
+﻿/*!
+ * \author Luis Diaz Jr
+ * \version 1.0
+ * \date 4-17-2017
+ *
+ * \mainpage Object Categories
+ */ 
+using UnityEngine;
 using System.Collections;
 
 public class ObjectCategory : MonoBehaviour {
 
-    // static variables
     public enum ROOMCODE
     {
         KITCHEN, BATHROOM, BEDROOM, LIVING, DINING, REC, OUTDOORS, NONE, ALL
-    }
+    }																				//!< defines values to categorize objects by room
+	
     public enum OBJECTTYPE
     {
         APPLIANCE, COMFORT, DECORATION, ELECTRONIC, KIDS, LIGHTING, PLUMBING, SURFACES, MISC, NONE, ALL
-    }
-    //public static int IDASSIGN = 1;
+    }																				//!< defines values to categorize objects by function
+    
+	
+	
+    public ROOMCODE roomType = ROOMCODE.KITCHEN;			//!< this object's room type
+	public OBJECTTYPE objectType = OBJECTTYPE.APPLIANCE;	//!< this object's function type
+	public int objectID;									//!< this object's ID
 
-    // object variables
-    public ROOMCODE roomType = ROOMCODE.KITCHEN;
-	public OBJECTTYPE objectType = OBJECTTYPE.APPLIANCE;
-	public int objectID;
-
+	
+	/*!
+	 * \brief [Unused]Returns this object's room type
+	 */
 	public ROOMCODE getRoomType() { return roomType; }	
 
 }
